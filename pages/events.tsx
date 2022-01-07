2c97c6df8749f3803dc1d4cde5899e9effb4b6e1
westshae <shaewest02@gmail.com>
2022-01-07 23:25:16 +1300

    /exec layout setup, placeholders put in

--- pages/events.tsx
+++ pages/events.tsx
@@ -2,18 +2,63 @@ import Container from "./components/Container";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
+import { Header, LI, Paragraph, UL } from "./components/Styles";
 
-
-const events = () =>{
-  return(
+const events = () => {
+  return (
     <Container>
-      <Navigation/>
+      <Navigation />
       <Content>
-        <h1>REE</h1>
+        <Header>Official 2022 VEC Events</Header>
+        <Paragraph>
+          Keep up to date with all the officially run events put on by the
+          Victoria Engineering Club!
+        </Paragraph>
+        <UL>
+          <LI>
+            <UL>
+              <LI><Header>Gaming Marathon</Header></LI>
+              <LI><Paragraph>When: TBD</Paragraph></LI>
+              <LI><Paragraph>Cost: TBD</Paragraph></LI>
+              <LI><Paragraph>Description: TBD</Paragraph></LI>
+            </UL>
+          </LI>
+          <br/>
+          <LI>
+            <UL>
+              <LI><Header>Victoria Hackfest</Header></LI>
+              <LI><Paragraph>When: TBD</Paragraph></LI>
+              <LI><Paragraph>Cost: TBD</Paragraph></LI>
+              <LI><Paragraph>Description: TBD</Paragraph></LI>
+            </UL>
+          </LI>
+          <br/>
+
+          <LI>
+            <UL>
+              <LI><Header>Game Jam</Header></LI>
+              <LI><Paragraph>When: TBD</Paragraph></LI>
+              <LI><Paragraph>Cost: TBD</Paragraph></LI>
+              <LI><Paragraph>Description: TBD</Paragraph></LI>
+            </UL>
+          </LI>
+          <br/>
+
+          <LI>
+            <UL>
+              <LI><Header>Pub Quiz</Header></LI>
+              <LI><Paragraph>When: TBD</Paragraph></LI>
+              <LI><Paragraph>Cost: TBD</Paragraph></LI>
+              <LI><Paragraph>Description: TBD</Paragraph></LI>
+            </UL>
+          </LI>
+          <br/>
+
+        </UL>
       </Content>
-      <Footer/>
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
 export default events;
--- pages/exec.tsx
+++ pages/exec.tsx
@@ -2,6 +2,7 @@ import Container from "./components/Container";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
+import { Header, LI, Paragraph, UL } from "./components/Styles";
 
 
 const exec = () =>{
@@ -9,7 +10,53 @@ const exec = () =>{
     <Container>
       <Navigation/>
       <Content>
-        <h1>REE</h1>
+      <Header>VEC 2022 Executive Team</Header>
+        <Paragraph>
+          Our volunteers which make sure our events are run, our funding is secured, and Howard's dad jokes are contained.
+        </Paragraph>
+        <UL>
+          <LI>
+            <UL>
+              <LI><Header>President</Header></LI>
+              <LI><Paragraph>Holder: Leon Bowie</Paragraph></LI>
+              <LI><Paragraph>Role:</Paragraph></LI>
+            </UL>
+          </LI>
+          <br/>
+          <LI>
+            <UL>
+              <LI><Header>Vice President</Header></LI>
+              <LI><Paragraph>Holder: Shae West</Paragraph></LI>
+              <LI><Paragraph>Role:</Paragraph></LI>
+            </UL>
+          </LI>
+          <br/>
+          <LI>
+            <UL>
+              <LI><Header>Treasurer</Header></LI>
+              <LI><Paragraph>Holder: Alex</Paragraph></LI>
+              <LI><Paragraph>Role:</Paragraph></LI>
+            </UL>
+          </LI>
+          <br/>
+          <LI>
+            <UL>
+              <LI><Header>Secretary</Header></LI>
+              <LI><Paragraph>Holder: Georgia Barrand</Paragraph></LI>
+              <LI><Paragraph>Role:</Paragraph></LI>
+            </UL>
+          </LI>
+          <br/>
+          <LI>
+            <UL>
+              <LI><Header>General Executives</Header></LI>
+              <LI><Paragraph>Holders:</Paragraph></LI>
+              <LI><Paragraph>1</Paragraph></LI>
+              <LI><Paragraph>2</Paragraph></LI>
+            </UL>
+          </LI>
+          <br/>
+        </UL>
       </Content>
       <Footer/>
     </Container>
